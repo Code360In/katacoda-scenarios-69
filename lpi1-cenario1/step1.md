f2123950c8da9d5ddd0cb61e62121cae824dd5c2
DaviAraujoCC <davi.araujo13356@gmail.com>
2021-09-09 16:07:38 -0300

    some changes

--- lpi1-cenario1/step1.md
+++ lpi1-cenario1/step1.md
@@ -1,3 +1,3 @@
-Use o comando `echo` para exibir a saída da variável <b>PATH</b> e <b>SHELL</b>.
+Use o comando `echo` para exibir a saída das variáveis <b>PATH</b> e <b>SHELL</b>.
 
-Após isso crie dois arquivos `path.txt` e `shell.txt` dentro do `/root` e insira a saída dos comandos em cada um respectivamente.
+Após insira a saída dos comandos nos arquivos `/root/lpi/path.txt` e `/root/lpi/shell.txt`.
--- lpi1-cenario1/verify_1.sh
+++ lpi1-cenario1/verify_1.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-verificar_path=$(cat /root/path.txt)
-verificar_shell=$(cat /root/shell.txt)
+verificar_path=$(cat /root/lpi/path.txt)
+verificar_shell=$(cat /root/lpi/shell.txt)
+path = "/root/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/root/.dotnet/tools"
+shell ="/bin/bash"
 
 
 if test -z "$verificar_path" ||  test -z "$verificar_shell"
 then
    exit 1
 else
-   if test "$verificar_path" == "$PATH" && test "$verificar_shell" == "$SHELL"
+   if test "$verificar_path" == "$path" && test "$verificar_shell" == "$shell"
    then
      echo "done"
      exit 0
