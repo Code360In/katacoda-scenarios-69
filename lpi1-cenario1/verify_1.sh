61b9039f1bb1e162593a289bbb7106f5e979584f
DaviAraujoCC <davi.araujo13356@gmail.com>
2021-09-09 16:14:41 -0300

    fix

--- lpi1-cenario1/verify_1.sh
+++ lpi1-cenario1/verify_1.sh
@@ -2,8 +2,8 @@
 
 verificar_path=$(cat /root/lpi/path.txt)
 verificar_shell=$(cat /root/lpi/shell.txt)
-path = "/root/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/root/.dotnet/tools"
-shell ="/bin/bash"
+path="/root/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/root/.dotnet/tools"
+shell="/bin/bash"
 
 
 if test -z "$verificar_path" ||  test -z "$verificar_shell"
